f6c1d10a45b1f386237bf1ed2d57c9f6cf240fba
Francesco Cattaneo <117264986+Fraaaaaunimib@users.noreply.github.com>
2026-04-20 14:07:38 +0000

    Modifiche

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,14 @@
 import org.ejml.data.DMatrixSparseCSC;
-import org.ejml.ops.MatrixIO;
+import java.io.File;
+import java.util.Scanner;
 import java.io.IOException;
 
-public class CaricaMatrice {
+public class Main {
     public static void main(String[] args) {
-        try {
-            // Legge il file .mtx e lo carica in una matrice sparsa
-            // DMatrixSparseCSC è il formato ideale per matrici con molti zeri
-            DMatrixSparseCSC matrice = MatrixIO.loadMatrixMarketDCSC("percorso/del/tuo/file.mtx");
-
-            System.out.println("Matrice caricata con successo!");
-            System.out.println("Righe: " + matrice.numRows);
-            System.out.println("Colonne: " + matrice.numCols);
-
-        } catch (IOException e) {
-            System.err.println("Errore durante la lettura del file: " + e.getMessage());
-        }
+       
     }
+
+    public static DMatrixSparseCSC caricaMatriceManualmente(String percorso) throws IOException {
+        
+    } 
 }
