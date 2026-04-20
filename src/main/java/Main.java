be65542ea35c18ddf58685ba2f079de04e8a7977
Francesco Cattaneo <117264986+Fraaaaaunimib@users.noreply.github.com>
2026-04-20 14:11:33 +0000

    Main semplice

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import org.ejml.data.DMatrixSparseCSC;
+import java.io.File;
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+
+    public static void caricaMatriceManualmente(String percorso) throws IOException {
+        
+    } 
+}
